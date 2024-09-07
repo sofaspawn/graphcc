31770db67d7aa09a342601de1be97e543d1dd19f
sofaspawn <h.m1nus.work@gmail.com>
2024-09-08 00:29:06 +0530

    grid is showing

--- main.c
+++ main.c
@@ -1,63 +1,85 @@
 #include <raylib.h>
 
-void boundCheck(int x, int y, int direction[]);
-
 const int screenWidth = 3000;
 const int screenHeight = 2000;
 
+const Color BGCOLOR = {46,46,46,100};
+float SCALE = 2.0;
+//const Color GRIDCOLOR = {255,255,255,100};
+
+void renderAxes(){
+    ClearBackground(BGCOLOR);
+
+    const int XAXIS[2][2] = {{0, screenHeight/2}, {screenWidth, screenHeight/2}};
+    const int YAXIS[2][2] = {{screenWidth/2, 0}, {screenWidth/2, screenHeight}};
+
+    DrawLine(XAXIS[0][0], XAXIS[0][1], XAXIS[1][0], XAXIS[1][1], WHITE);
+    DrawLine(YAXIS[0][0], YAXIS[0][1], YAXIS[1][0], YAXIS[1][1], WHITE);
+}
+
+void renderGrid(RenderTexture2D *target){
+    DrawTextureRec(target->texture, (Rectangle){0, 0, (float)target->texture.width, (float)target->texture.height}, (Vector2){0,0}, BGCOLOR);
+}
+
+void computeFunction(){
+}
+
+void renderFunction(RenderTexture2D *target){
+
+}
+
+void gridTexture(RenderTexture2D *target){
+    BeginTextureMode(*target);
+    ClearBackground(BGCOLOR);
+
+    int inc = 10;
+
+    for(int i=0; i<screenWidth; i+=inc*SCALE){
+        DrawLine(i, 0, i, screenHeight, WHITE);
+    }
+
+    for(int i=0; i<screenHeight; i+=inc*SCALE){
+        DrawLine(0, i, screenWidth, i, WHITE);
+    }
+
+    EndTextureMode();
+}
+
 int main(void){
 
-    InitWindow(screenWidth, screenHeight, "example window");
+    InitWindow(screenWidth, screenHeight, "draw graphs and shit");
 
-    SetTargetFPS(60);
+    SetTargetFPS(90);
 
-    int x = screenWidth/2;
-    int y = screenHeight/2;
+    RenderTexture2D target = LoadRenderTexture(screenWidth, screenHeight);
 
-    int velocity = 20;
-    int direction[2] = {0,0};
+    gridTexture(&target);
 
     while(!WindowShouldClose()){
 
         if(IsKeyPressed(KEY_Q)){
             break;
-        } else if (IsKeyDown(KEY_UP)){
-            direction[0] = 0;
-            direction[1] = -1;
-        } else if (IsKeyDown(KEY_DOWN)){
-            direction[0] = 0;
-            direction[1] = 1;
-        } else if (IsKeyDown(KEY_LEFT)){
-            direction[0] = -1;
-            direction[1] = 0;
-        } else if (IsKeyDown(KEY_RIGHT)){
-            direction[0] = 1;
-            direction[1] = 0;
+        }
+        if(IsKeyPressed(KEY_EQUAL)){
+            //TODO: implement the render texture function to call it when changing scale
+            SCALE+=0.5;
+            gridTexture(&target);
+        }
+        if(IsKeyPressed(KEY_MINUS)){
+            if(SCALE>0){
+                SCALE-=0.5;
+                gridTexture(&target);
+            }
         }
 
-        x+=direction[0]*velocity;
-        y+=direction[1]*velocity;
-
-        boundCheck(x, y, direction);
-
         BeginDrawing();
-        ClearBackground(BLACK);
-
-        DrawCircle(x, y, 100, RAYWHITE);
-
+        renderAxes();
+        renderGrid(&target);
         EndDrawing();
     }
 
+    UnloadRenderTexture(target);
     CloseWindow();
 
     return 0;
-}
-
-void boundCheck(int x, int y, int direction[]){
-    if (x>=screenWidth || x<=0){
-        direction[0] *= -1;
-    }
-    if (y>=screenHeight || y<=0){
-        direction[1] *= -1;
-    }
 }
